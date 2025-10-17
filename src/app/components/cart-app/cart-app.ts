5132fa9d630a886c41dff37e7be48a1eeb2e5945
Sergey Brovko <serbro7@gmail.com>
2025-10-17 09:46:42 +0200

    Products: products called upon an shown on screen

--- src/app/components/cart-app/cart-app.ts
+++ src/app/components/cart-app/cart-app.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product';
 
 @Component({
   selector: 'cart-app',
   imports: [],
   templateUrl: './cart-app.html'
 })
-export class CartAppComponent {
+export class CartAppComponent implements OnInit{
+
+  products: Product[] = [];
+
+  constructor(private service: ProductService) {
+
+  }
+
+  //When the application gets initialised, the product service that was previously initialised gets called
+  //and populates our empty Product array with products returned by the service
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
 
 }
